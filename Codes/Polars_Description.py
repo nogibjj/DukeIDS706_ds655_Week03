52aa3764545a0fe5c8152722b215f4949a76970f
Divya Sharma <143483773+DivyaSharma0795@users.noreply.github.com>
2023-09-14 16:50:06 +0000

    Updating codes to Polars instead of Pandas

--- Codes/Polars_Description.py
+++ Codes/Polars_Description.py
@@ -1,6 +1,7 @@
 #  IDS 706 Week 2 assignment - Creating a function to show descriptive statistics for a dataframe
 # Convert this pandas code to corresponding polars code
 
-def PandasDesc(df):
-  return df.describe()
+import polars as pl
 
+def PolarsDesc(df):
+  return df.describe()
