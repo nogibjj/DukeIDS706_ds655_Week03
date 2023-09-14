1cbf278f7c9662243c74367590a148543e4a9bc6
Divya Sharma <143483773+DivyaSharma0795@users.noreply.github.com>
2023-09-14 16:34:04 +0000

    Added Polars to requirements

--- Codes/Pandas_Description.py
+++ Codes/Pandas_Description.py
@@ -1,5 +1,11 @@
 #  IDS 706 Week 2 assignment - Creating a function to show descriptive statistics for a dataframe
-#import pandas as pd
+# Convert this pandas code to corresponding polars code
+
+import polars as pl
+
+def PolarsDesc(df):
+  return df.describe()
 
 def PandasDesc(df):
   return df.describe()
+
