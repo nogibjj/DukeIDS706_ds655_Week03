1f67281414562a77ac253768fb28b4178d5f48a0
Divya Sharma <143483773+DivyaSharma0795@users.noreply.github.com>
2023-09-15 18:58:42 +0000

    Updated the ScatterPlot to incorporate Polars Dataframe

--- Codes/Polars_Plot.py
+++ Codes/Polars_Plot.py
@@ -1,17 +1,19 @@
 #   Importing Packages
 import polars as pl
 import seaborn as sns
-import plotly.express as px
+import matplotlib.pyplot as plt
 
-DataSource_Link = "https://raw.githubusercontent.com/Opensourcefordatascience/Data-sets/master/Iris_Data.csv"
-df_s = pl.read_csv(DataSource_Link)
-#Plot a scatterplot of sepal length vs sepal width
+def PolarsPlot(df):
+    #   Getting the x-axis and y-axis for the plot
+    slen = df.select(pl.col('sepal_length')).to_numpy().ravel()
+    swid = df.select(pl.col('sepal_width')).to_numpy().ravel()
+    #   Adding colours based on the Species column
+    hue  = df.select(pl.col('species')).to_numpy().ravel()
+    #   Plotting the Scatter chart
+    ax = sns.scatterplot(x=slen, y=swid, hue=hue)
+    #   Saving the chart as a png file in the resources folder
+    plt.savefig("./Resources/Data_Plot.png")
+    print("Successfully saved the plot in Resources folder")
 
 
 
-slen = df_s.select(pl.col('sepal_length')).to_numpy().ravel()
-swid = df_s.select(pl.col('sepal_width')).to_numpy().ravel()
-hue  = df_s.select(pl.col('species')).to_numpy().ravel()
-sns.scatterplot(x=slen, y=swid, hue=hue, data = df_s)
-
-sns.show()
--- Codes/Test_Week3_Polars.py
+++ Codes/Test_Week3_Polars.py
@@ -11,15 +11,14 @@ def test_Polars():
     df_s = pl.read_csv(DataSource_Link)
 
     # Writing the summary statistics to a file Summary.md in output folder
-    Desc_df = PolarsDesc(df_s)
     with open("./Resources/Summary.md", "w", encoding="utf-8") as f:
-        f.write(str(Desc_df))
+        f.write(str(PolarsDesc(df_s)))
+        print("Successfuessfully written the Summary.md file in the Resources folder")
 
     # Pasting the sample graph in the output folder
-    # PandasPlot(df_s)
     PolarsPlot(df_s)
-    #   Reading the Reference Data from the Resources Folder
 
+    #   Reading the Reference Data from the Resources Folder
     DataReference_Link = "./Resources/Iris_Data.csv"
     df_r = pl.read_csv(DataReference_Link)
 
