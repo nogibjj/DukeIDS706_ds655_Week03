00c24b892aa2cf3b386d462e9d2c11ad7a7807bc
Divya Sharma <divya.sharma@duke.edu>
2023-09-20 17:34:17 -0400

    Update Polars_Plot.py

--- Codes/Polars_Plot.py
+++ Codes/Polars_Plot.py
@@ -2,6 +2,8 @@
 import polars as pl
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+import datetime
 
 def PolarsPlot(df):
     #   Getting the x-axis and y-axis for the plot
@@ -11,7 +13,8 @@ def PolarsPlot(df):
     hue  = df.select(pl.col('species')).to_numpy().ravel()
     #   Plotting the Scatter chart
     ax = sns.scatterplot(x=slen, y=swid, hue=hue)
-    ax.set(xlabel ="Sepal Length", ylabel = "Sepal Width", title ='Distribution of Sepal Length and Sepal Width across different Species')
+    ax.set(xlabel ="Sepal Length", ylabel = "Sepal Width", title ='Distribution of Sepal Length and Sepal Width across different Species \n Generated on - '
+        + str(datetime.datetime.now()))
     #   Saving the chart as a png file in the resources folder
     plt.savefig("./Resources/Data_Plot.png")
     
